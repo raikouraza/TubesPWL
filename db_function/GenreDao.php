662f1d7719b8a4b8aa8052b5f2a06ae69663953d
Stefanus Hermawan <it1772023@student.it.maranatha.edu>
2019-11-28 16:52:34 +0700

    Fixbug MemberDao & Update GenreDao

--- db_function/GenreDao.php
+++ db_function/GenreDao.php
@@ -1,54 +1,30 @@
 <?php
 
-class MemberDao
-{
-    function login(Member $member)
-    {
-        $link = DBHelper::createMySQLConnection();
-        $query = "SELECT * FROM tbmember WHERE member_username=? AND member_password=? LIMIT 1";
-        $stmt = $link->prepare($query);
-        $stmt->bindValue(1, $member->getMemberUsername(), PDO::PARAM_STR);
-        $stmt->bindValue(2, $member->getMemberPassword(), PDO::PARAM_STR);
-        $stmt->execute();
-        $result = $stmt->fetchObject("Member");
-        $link = null;
-        return $result;
-    }
+class GenreDao{
 
-
-    function getAllMember()
+    function getAllGenre()
     {
         $link = DBHelper::createMySQLConnection();
 
-        $query = "SELECT * FROM tbmember";
+        $query = "SELECT * FROM tbgenre";
 
         $result = $link->query($query);
 
-        $result->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Member");
+        $result->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Genre");
 
         return $result;
 
     }
 
-    function addMember(Member $member)
+    function addGenre(Genre $genre)
     {
         $link = DBHelper::createMySQLConnection();
         $link->beginTransaction();
-        $query = "INSERT INTO tbmember(member_username,
-                                 member_password,
-                                 member_email,
-                                 member_nama_depan,
-                                 member_nama_belakang,
-                                 member_photo) 
-                                 VALUES(?,?,?,?,?,?)";
+        $query = "INSERT INTO tbgenre(genre_name) VALUES(?)";
 
         $stmt = $link->prepare($query);
-        $stmt->bindValue(2, $member->getMemberUsername(), PDO::PARAM_STR);
-        $stmt->bindValue(3, $member->getMemberPassword(), PDO::PARAM_STR);
-        $stmt->bindValue(4, $member->getMemberEmail(), PDO::PARAM_INT);
-        $stmt->bindValue(4, $member->getMemberNamaDepan(), PDO::PARAM_INT);
-        $stmt->bindValue(4, $member->getMemberNamaBelakang(), PDO::PARAM_INT);
-        $stmt->bindValue(4, $member->getMemberPhoto(), PDO::PARAM_INT);
+        $stmt->bindValue(1, $genre->getGenreUsername(), PDO::PARAM_STR);
+
         if ($stmt->execute()) {
             $link->commit();
         } else {
@@ -59,14 +35,14 @@ class MemberDao
 
     }
 
-    function deleteMember(Member $member)
+    function deleteGenre(Genre $genre)
     {
         $link = DBHelper::createMySQLConnection();
         $link->beginTransaction();
-        $query = "DELETE FROM tbmember WHERE member_id=?";
+        $query = "DELETE FROM tbgenre WHERE genre_id=?";
 
         $stmt = $link->prepare($query);
-        $stmt->bindValue(1, $member->getMemberId(), PDO::PARAM_INT);
+        $stmt->bindValue(1, $genre->getGenreId(), PDO::PARAM_INT);
         if ($stmt->execute()) {
             $link->commit();
         } else {
@@ -77,24 +53,17 @@ class MemberDao
 
     }
 
-// member tidak bisa mengganti username
-    function updateMember(Member $member)
+// genre tidak bisa mengganti username
+    function updateGenre(Genre $genre)
     {
         $link = DBHelper::createMySQLConnection();
         $link->beginTransaction();
-        $query = "UPDATE tbmember SET   member_email=?,
-                                    member_nama_depan=?,
-                                    member_nama_belakang=?,
-                                    member_photo=?  
-                                    WHERE member_id=?";
+        $query = "UPDATE tbgenre SET genre_name=?
+                                    WHERE genre_id=?";
 
         $stmt = $link->prepare($query);
-        $stmt->bindValue(1, $member->getMemberEmail(), PDO::PARAM_STR);
-        $stmt->bindValue(2, $member->getMemberNamaDepan(), PDO::PARAM_INT);
-        $stmt->bindValue(2, $member->getMemberNamaBelakang(), PDO::PARAM_INT);
-        $stmt->bindValue(2, $member->getMemberPhoto(), PDO::PARAM_INT);
-
-        $stmt->bindValue(3, $member->getMemberId(), PDO::PARAM_INT);
+        $stmt->bindValue(1, $genre->getGenreName(), PDO::PARAM_STR);
+        $stmt->bindValue(2, $genre->getGenreId(), PDO::PARAM_INT);
         if ($stmt->execute()) {
             $link->commit();
         } else {
@@ -104,37 +73,18 @@ class MemberDao
         $link = null;
 
     }
-
-    function updatePassword(Member $member)
-    {
-        $link = DBHelper::createMySQLConnection();
-        $link->beginTransaction();
-        $query = "UPDATE tbmember SET member_password=? WHERE member_id=?";
-
-        $stmt = $link->prepare($query);
-        $stmt->bindValue(1, $member->getMemberPassword(), PDO::PARAM_STR);
-        $stmt->bindValue(2, $member->getMemberId(), PDO::PARAM_INT);
-        if ($stmt->execute()) {
-            $link->commit();
-        } else {
-            $link->rollBack();
-        }
-
-        $link = null;
-
-    }
-
-    function getMemberById(Member $member)
+    
+    function getGenreById(Genre $genre)
     {
         $link = DBHelper::createMySQLConnection();
 
-        $query = "SELECT * FROM tbmember WHERE member_id = ? LIMIT 1";
+        $query = "SELECT * FROM tbgenre WHERE genre_id = ? LIMIT 1";
 
         $stmt = $link->prepare($query);
-        $stmt->bindValue(1, $member->getMemberId(), PDO::PARAM_INT);
+        $stmt->bindValue(1, $genre->getGenreId(), PDO::PARAM_INT);
         $stmt->execute();
 
-        $result = $stmt->fetchObject("Member");
+        $result = $stmt->fetchObject("Genre");
 
         $link = null;
 

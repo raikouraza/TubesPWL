1ddbc3f0ae9f5b4289ebc468e3fbb17048b0b46c
Stefanus Hermawan <it1772023@student.it.maranatha.edu>
2019-11-28 15:37:34 +0700

    ngerjain Entity

--- entity/Member.php
+++ entity/Member.php
@@ -1,7 +1,125 @@
 <?php
 
 
-class Member
-{
+class Member{
+    private $member_id;
+    private $member_username;
+    private $member_password;
+    private $member_email;
+    private $member_nama_depan;
+    private $member_nama_belakang;
+    private $member_photo;
+
+    /**
+     * @return mixed
+     */
+    public function getMemberId()
+    {
+        return $this->member_id;
+    }
+
+    /**
+     * @param mixed $member_id
+     */
+    public function setMemberId($member_id)
+    {
+        $this->member_id = $member_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemberUsername()
+    {
+        return $this->member_username;
+    }
+
+    /**
+     * @param mixed $member_username
+     */
+    public function setMemberUsername($member_username)
+    {
+        $this->member_username = $member_username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemberPassword()
+    {
+        return $this->member_password;
+    }
+
+    /**
+     * @param mixed $member_password
+     */
+    public function setMemberPassword($member_password)
+    {
+        $this->member_password = $member_password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemberEmail()
+    {
+        return $this->member_email;
+    }
+
+    /**
+     * @param mixed $member_email
+     */
+    public function setMemberEmail($member_email)
+    {
+        $this->member_email = $member_email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemberNamaDepan()
+    {
+        return $this->member_nama_depan;
+    }
+
+    /**
+     * @param mixed $member_nama_depan
+     */
+    public function setMemberNamaDepan($member_nama_depan)
+    {
+        $this->member_nama_depan = $member_nama_depan;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemberNamaBelakang()
+    {
+        return $this->member_nama_belakang;
+    }
+
+    /**
+     * @param mixed $member_nama_belakang
+     */
+    public function setMemberNamaBelakang($member_nama_belakang)
+    {
+        $this->member_nama_belakang = $member_nama_belakang;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemberPhoto()
+    {
+        return $this->member_photo;
+    }
+
+    /**
+     * @param mixed $member_photo
+     */
+    public function setMemberPhoto($member_photo)
+    {
+        $this->member_photo = $member_photo;
+    }
 
 }
--- entity/User.php
+++ entity/User.php
@@ -1,7 +1,74 @@
 <?php
 
 
-class User
-{
+class User{
+    private $user_id;
+    private $user_username;
+    private $user_password;
+    private $user_role;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserUsername()
+    {
+        return $this->user_username;
+    }
+
+    /**
+     * @param mixed $user_username
+     */
+    public function setUserUsername($user_username)
+    {
+        $this->user_username = $user_username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserPassword()
+    {
+        return $this->user_password;
+    }
+
+    /**
+     * @param mixed $user_password
+     */
+    public function setUserPassword($user_password)
+    {
+        $this->user_password = $user_password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserRole()
+    {
+        return $this->user_role;
+    }
+
+    /**
+     * @param mixed $user_role
+     */
+    public function setUserRole($user_role)
+    {
+        $this->user_role = $user_role;
+    }
 
 }

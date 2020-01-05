3dfdaf33535b7c067c38e9f1c4be5830229f608d
raikouraza <45711484+raikouraza@users.noreply.github.com>
2020-01-05 18:28:14 +0700

    Showing list of movies is now working
    Showing list of movies is now working

--- index.php
+++ index.php
@@ -59,7 +59,6 @@ if (!isset($_SESSION['user_logged'])) {
 <!-- Navigation -->
 <!-- Jika User Sudah Login -->
 <?php if ($_SESSION['user_logged']) { ?>
-    <h4> <?php echo "Selamat Datang" . $_SESSION['name'] ?> </h4>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="?menu=hm">CINEMA YAS!</a>
--- view/movies.php
+++ view/movies.php
@@ -4,21 +4,46 @@ $numOfCols = 4;
 $rowCount = 0;
 $bootstrapColWidth = 12 / $numOfCols;
 ?>
-<div class="row">
-    <?php
-    $filmDao = new FilmDao();
-    $films = $filmDao->getAllFilm();
-    /* @var $film film */
-    foreach ($films as $film){
-        ?>
-        <div class="col-md-<?php echo $bootstrapColWidth; ?>">
-            <div class="thumbnail">
-                <?php echo $film->getFilmJudul(); ?>">
-            </div>
-        </div>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style type="text/css">
+        div.container-fluid {
+            width: 70%;
+            margin: auto;
+
+            background-color: darkgrey;
+        }
+    </style>
+
+</head>
+<body>
+<p align="center" style="font-size: 40pt;background-color: black;">NOW SHOWING</p>
+<div class="container-fluid">
+    <div class="row" >
         <?php
-        $rowCount++;
-        if($rowCount % $numOfCols == 0) echo '</div><div class="row">';
-    }
-    ?>
+        $filmDao = new FilmDao();
+        $films = $filmDao->getAllFilm();
+        /* @var $film film */
+        foreach ($films as $film){
+            ?>
+            <div style="padding-top: 10px;" align="center" class="col-md-<?php echo $bootstrapColWidth; ?>">
+                <div class="thumbnail">
+                    <?php echo "<img src='".$film->getFilmPoster()."' height='350' width='235'' />";?>
+                </div>
+            </div>
+            <?php
+            $rowCount++;
+            if($rowCount % $numOfCols == 0) echo '</div><div class="row">';
+        }
+        ?>
+    </div>
 </div>
+</body>
+</html>

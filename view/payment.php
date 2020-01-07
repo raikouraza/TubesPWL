2c095434fa66efcdfef1656e74bd57376063a66b
raikouraza <45711484+raikouraza@users.noreply.github.com>
2020-01-07 11:30:58 +0700

    Transaksi

--- view/payment.php
+++ view/payment.php
@@ -50,6 +50,7 @@ ob_start();
             max-width: 100%;
             max-height: 100%;
         }
+
         img {
             max-width: 100%;
             max-height: 100%;
@@ -60,11 +61,12 @@ ob_start();
 
 <!-- Navigation -->
 <!-- Jika User Sudah Login -->
-<?php if($_SESSION['user_logged']){?>
+<?php if ($_SESSION['user_logged']){ ?>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="?menu=hm">CINEMA YAS!</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -74,7 +76,7 @@ ob_start();
                             <span class="sr-only">(current)</span>
                         </a>
                     </li>
-                    <li class="nav-item" >
+                    <li class="nav-item">
                         <a class="nav-link" href="?menu=mov">Movies</a>
                     </li>
 
@@ -140,77 +142,28 @@ ob_start();
 <!-- Page Content -->
 
 <div class="row">
-    <div class="col-sm-12">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8">
         <div class="card">
             <div class="card-body">
-                <h5>Pembayaran Tiket XD</h5>
+                <h1 align="center" style="color: #45a29e">Pembayaran Tiket </h1>
                 <br>
                 <div class="row">
                     <div class="col-md-6">
                         <form method="post" enctype="multipart/form-data" id="filmForm">
-                            <div class="form-group">
-                                <label for="txtJudulFilm">Masukkan Judul</label>
-                                <hr>
-                                <input type="text" class="form-control" id="txtjudul" name="txtFilmJudul"
-                                       placeholder="Masukkan Judul">
-                            </div>
                             <br>
                             <div class="form-group">
-                                <label for="txtDeskripsi">Masukkan Deskripsi</label>
-                                <hr>
-                                <textarea class="form-control" rows="5" form="filmForm" name="txtFilmDeskripsi"
-                                          placeholder="Masukan Deskripsi"></textarea>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="txtPoster">Masukkan Poster</label>
-                                <hr>
-                                <input type="file" class="form-control-file" id="txtPoster" name="txtFilmPoster">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="txtTrailer">Masukkan Link Video</label>
-                                <hr>
-                                <input type="url" class="form-control" id="txtTrailer" name="txtFilmTrailer"
-                                       placeholder="Masukkan Link Video">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="txtSutradara">Masukkan Nama Sutradara</label>
+                                <label for="txtSutradara">Tiket ID</label>
                                 <hr>
                                 <input type="text" class="form-control" id="txtSutradara" name="txtFilmSutradara"
-                                       placeholder="Masukkan Nama Sutradara">
+                                       placeholder="Masukkan Id Tiket">
                             </div>
                             <br>
                             <div class="form-group">
-                                <label for="txtNamaAktor">Masukkan Nama-Nama Aktor</label>
+                                <label for="txtNamaAktor">Jadwal ID</label>
                                 <hr>
                                 <input type="text" class="form-control" id="txtAktor" name="txtFilmAktor"
-                                       placeholder="Masukkan Nama-Nama Aktor">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="txtDurasi">Masukkan Durasi</label>
-                                <hr>
-                                <input type="text" class="form-control" id="txtDurasi" name="txtFilmDurasi"
-                                       placeholder="Masukkan Durasi">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="txtRating">Pilih Rating</label>
-                                <hr>
-                                <label class="radio-inline"><input value="SU" type="radio" name="txtFilmRating" checked>SU</label>
-                                <label class="radio-inline"><input value="BO" type="radio"
-                                                                   name="txtFilmRating">BO</label>
-                                <label class="radio-inline"><input value="R" type="radio" name="txtFilmRating">R</label>
-                                <label class="radio-inline"><input value="D" type="radio" name="txtFilmRating">D</label>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="txtNamaAktor">Masukkan Genre Film</label>
-                                <hr>
-                                <input type="text" class="form-control" name="txtFilmGenre"
-                                       placeholder="Masukkan Genre">
+                                       placeholder="Masukkan Jadwal ID">
                             </div>
                             <br>
                             <button type="submit" class="btn btn-primary" id="btnSubmit" name="btnSubmit">Submit
@@ -225,5 +178,6 @@ ob_start();
             </div>
         </div>
     </div>
+    <div class="col-sm-2"></div>
 </div>
 <!-- [ Main Content ] end -->
